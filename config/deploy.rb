999e42a2c6e0944465ff0b6827428cbb1518b3e8
Kem Tardif <tardif.kremlin@gmail.com>
2020-10-16 15:22:32 -0400

    2nd Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.14.1"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "kemtardif"
+set :application, "KemTardif"
 set :repo_url, "https://github.com/kemtardif/Rocket-Elevators-Information-System.git"
 
 # Default branch is :master
